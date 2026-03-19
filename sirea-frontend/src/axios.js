dc8f7d8b96b697ae3d54ddf4447bfd6ed240cb88
Cristian Figueroa Ferrer <ilandbz@gmail.com>
2026-03-18 19:46:22 -0500

    asd

--- sirea-frontend/src/axios.js
+++ sirea-frontend/src/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://sirea-api.test/api'; // Cambia por tu URL local
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
-// Si tenemos un token guardado, lo inyectamos en las cabeceras automáticamente
 const token = localStorage.getItem('token');
 if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
